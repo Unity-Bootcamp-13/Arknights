c0da8b6cf07c6a75474768d2714dfa79fbf836bb
wjdtmdgns0905 <jqhrtn33@gmail.com>
2025-06-27 11:27:17 +0900

    [Feat] 구조체, 인터페이스, 상수, 열거형 모음 cs 파일 생성
    1. 구조체 모음
    2. 인터페이스 모음
    3. 상수 모음
    4. 열거형 모음

---
+++ ZooDOS/Assets/02.Scripts/Common/Interfaces.cs
@@ -0,0 +1,12 @@
+﻿// 인터페이스 모음
+public interface IAttackable
+{
+    public Unit FindTarget();
+    public bool IsInRange(Unit unit);
+}
+
+public interface IDamagable
+{
+    public void GetDamage();
+}
+
---
+++ ZooDOS/Assets/02.Scripts/Common/Types.cs
@@ -0,0 +1,9 @@
+﻿// enum 모음
+
+public enum PlayerUnitType
+{
+    Nothing,
+    Defender,
+    Sniper,
+    Medic,
+}
