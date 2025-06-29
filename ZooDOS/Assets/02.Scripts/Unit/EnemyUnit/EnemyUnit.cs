aaf1ef7235c23b61bf54c2c68917a88b86e9840c
volcano1412 <55967466+volcano1412@users.noreply.github.com>
2025-06-30 02:41:21 +0900

    [Feat] 적리스트 저장방식 변경
    적리스트 저장방식 변경

--- ZooDOS/Assets/02.Scripts/Unit/EnemyUnit/EnemyUnit.cs
+++ ZooDOS/Assets/02.Scripts/Unit/EnemyUnit/EnemyUnit.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class EnemyUnit : MonoBehaviour
 {
+    //적스포너
     public EnemyUnitSpawner _spawner;
+    
+    //현재 있는 타일
+    public EnemyTile currentTile;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,21 +25,31 @@ public class EnemyUnit : MonoBehaviour
         _spawner = spawner;
     }
     
+    //이동시 위치 업데이트
+    public void UpdateTile(Vector2Int vec2)
+    {
+        currentTile.enemiesOnTile.Remove(this);
+        currentTile = _spawner.enemyTileMap[vec2.x, vec2.y];
+        currentTile.enemiesOnTile.Add(this);
+    }
+    
     
     //저지당하면 정지
     
     
     
     
-    //풀리면 다시 경로탐색
+    //저지 풀리면 다시 경로탐색 
+    
     
     
     
     //죽으면 EnemyList에서 Remove 요청
+    [ContextMenu("OnDead")]
     public void OnDead()
     {
-        // 죽을 때 자기 자신을 제거 요청
-        _spawner.RemoveEnemy(this);
+        // 죽을 때 적리스트에서 자기 자신을 제거 요청
+        currentTile.enemiesOnTile.Remove(this);
         Destroy(gameObject);
     }
     
--- ZooDOS/Assets/02.Scripts/Unit/EnemyUnit/EnemyUnitSpawner.cs
+++ ZooDOS/Assets/02.Scripts/Unit/EnemyUnit/EnemyUnitSpawner.cs
@@ -1,18 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
-
+public class EnemyTile
+{
+    public List<EnemyUnit> enemiesOnTile = new();
+}
 public class EnemyUnitSpawner : MonoBehaviour
 {
-    private List<EnemyUnit> enemyList;
-
+    public EnemyTile[,] enemyTileMap;
     
-    public IReadOnlyList<EnemyUnit> enemys => enemyList.AsReadOnly();
+    //public IReadOnlyList<EnemyUnit> enemys => enemyList.AsReadOnly();
     
     [SerializeField] private EnemySpawnData enemy;
+
+    private float i=0;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        enemyList = new List<EnemyUnit>();
+        int width = 10;
+        int height = 10;
+        enemyTileMap = new EnemyTile[10, 10];
+
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                enemyTileMap[x, y] = new EnemyTile();
+            }
+        }
         
         SpawnEnemy(enemy);
     }
@@ -20,15 +35,7 @@ public class EnemyUnitSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float i = 0;
-        if (i>1)
-        {
-            
-            Debug.Log(enemyList);
-            i = 0;
-        }
-
-        i += Time.deltaTime;
+        
     }
     
     
@@ -51,7 +58,8 @@ public class EnemyUnitSpawner : MonoBehaviour
     {
         var enemy = Instantiate(enemyData.enemyPrefab).GetComponent<EnemyUnit>();
         enemy.Initialize(this);
-        enemyList.Add(enemy);
+        
+        //enemyList.Add(enemy);
 
     }
     
@@ -59,9 +67,17 @@ public class EnemyUnitSpawner : MonoBehaviour
     //유닛이 죽으면 리스트에서 제거
     public void RemoveEnemy(EnemyUnit enemy)
     {
-        enemyList.Remove(enemy);
+        
+        //enemyList.Remove(enemy);
     }
     
+    //적리스트 타일좌표값으로 리턴
+    public List<EnemyUnit> OnTileEnemyList()
+    {
+        
+        
+        return new List<EnemyUnit>();
+    }
     
     //경로 시각화
     public void PathVisualization()
