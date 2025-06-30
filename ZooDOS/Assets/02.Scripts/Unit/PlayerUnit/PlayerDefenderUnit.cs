00e7f1bf90ae793ec447c4a85727db19cfd046a5
wjdtmdgns0905 <jqhrtn33@gmail.com>
2025-06-30 11:22:07 +0900

    Reapply "[Fix] 스크립트 분리, 공격모션 수정"
    This reverts commit bc7835dc78931e48f256ce81bc876c0b569c1eee.

--- ZooDOS/Assets/02.Scripts/Unit/PlayerUnit/PlayerDefenderUnit.cs
+++ ZooDOS/Assets/02.Scripts/Unit/PlayerUnit/PlayerDefenderUnit.cs
@@ -14,6 +14,7 @@ public class PlayerDefenderUnit : PlayerUnit, IAttackable
 
     protected void Update()
     {
+        // 공격 딜레이
         _leftAttackTime += Time.deltaTime;
 
         if (_leftAttackTime < _atkSpeed)
