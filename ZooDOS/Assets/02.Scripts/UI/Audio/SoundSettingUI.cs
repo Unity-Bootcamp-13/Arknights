0c0b1159f99e964e9a7e77df75773c98059c832a
nellmnellm <ebhegh1@naver.com>
2025-07-10 16:45:14 +0900

    [Fix] 사운드 리팩토링 후 옵션창 디테일 추가
    옵션창에 사운드 온오프 토글도 넣고 타이틀로 / 인게임으로 를 넣고 약간의 런타임 에러를 수정했습니다

--- ZooDOS/Assets/02.Scripts/UI/Audio/SoundSettingUI.cs
+++ ZooDOS/Assets/02.Scripts/UI/Audio/SoundSettingUI.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SoundSettingUI : MonoBehaviour
@@ -16,6 +17,10 @@ public class SoundSettingUI : MonoBehaviour
     [SerializeField] private TMP_Text _sfxText;
 
     [SerializeField] private AudioManager _audioManager;
+    [SerializeField] private Canvas _canvas;
+    [SerializeField] private GameSpeedController _gameSpeedController;  
+    private Button _blockerButton;
+
 
     private bool _visible = false;
     private void Start()
@@ -48,25 +53,76 @@ public class SoundSettingUI : MonoBehaviour
             _audioManager.SetVolume("SFX", v);
             _sfxText.text = ToPercent(v);
         });
-
+        CreateBlocker();
+        _blockerButton.gameObject.SetActive(false);
         _soundSettingPanel.SetActive(false);
+        
+        
     }
 
-    void InitSlider(Slider slider, TMP_Text volume, float init)
+    private void InitSlider(Slider slider, TMP_Text volume, float init)
     {
         slider.SetValueWithoutNotify(init);
         volume.text = ToPercent(init);
     }
 
 
-    void TogglePanel()
+    public void TogglePanel()
     {
         _visible = !_visible;
         _soundSettingPanel.SetActive(_visible);
+        _blockerButton.gameObject.SetActive(_visible);
+        if (_visible)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            _gameSpeedController.UpdateTimeScale();
+        }
     }
 
     static string ToPercent(float v)
     {
         return $"{Mathf.RoundToInt(v * 100)} %";
+    }
+
+    private void CreateBlocker()
+    {
+        // 1) 빈 오브젝트 + 컴포넌트
+        GameObject go = new("UnitInfoBlocker",
+                            typeof(RectTransform),
+                            typeof(CanvasRenderer),
+                            typeof(Image),
+                            typeof(Button));
+
+        // 2) Canvas 하위(Panel과 같은 부모)에 붙이기
+        go.transform.SetParent(_canvas.transform, false);
+
+        // 3) 풀스크린으로 Stretch
+        RectTransform rt = go.GetComponent<RectTransform>();
+        rt.anchorMin = Vector2.zero;
+        rt.anchorMax = Vector2.one;
+        rt.offsetMin = rt.offsetMax = Vector2.zero;
+
+        // 4) 이미지 → 완전 투명
+        Image img = go.GetComponent<Image>();
+        img.color = new Color(0, 0, 0, 0);
+        img.raycastTarget = true;
+
+        // 5) 버튼 클릭 → ClosePanel
+        _blockerButton = go.GetComponent<Button>();
+        _blockerButton.onClick.AddListener(TogglePanel);
+
+        // 6) 패널보다 뒤(인덱스가 작아야 뒤)로 이동
+        int panelIndex = _soundSettingPanel.transform.GetSiblingIndex();
+        go.transform.SetSiblingIndex(panelIndex);
+        _soundSettingPanel.transform.SetSiblingIndex(panelIndex + 1);
+
+    }
+
+    public void GotoMainLobby()
+    {
+        SceneManager.LoadScene("MainLobbyScene");
     }
 }
